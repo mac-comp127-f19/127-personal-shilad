368901a6d7c351233e0b9a352559b1feb7c0e523
Shilad Sen <shilad@gmail.com>
2019-09-11 13:41:02 -0500

    Added examples.

---
+++ test/stringPractice/MakeEmphaticTest.java
@@ -0,0 +1,25 @@
+package stringPractice;
+
+import activityStarterCode.stringPractice.MakeEmphatic;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class MakeEmphaticTest {
+
+    @Test
+    public void longStringTest() {
+        assertEquals("EMPHASIS!!!", MakeEmphatic.makeEmphatic("emphasis"));
+    }
+
+
+    @Test
+    public void shortStringTest() {
+        assertEquals("I!!!", MakeEmphatic.makeEmphatic("i"));
+    }
+
+    @Test
+    public void emptyStringTest() {
+        assertEquals("!!!", MakeEmphatic.makeEmphatic(""));
+    }
+}
---
+++ test/stringPractice/MakeSnakeTest.java
@@ -0,0 +1,19 @@
+package stringPractice;
+
+import activityStarterCode.stringPractice.MakeSnake;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class MakeSnakeTest {
+
+    @Test
+    public void mixedSsTest() {
+        assertEquals("SSSnake sssoundsss", MakeSnake.makeSnake("Snake sounds"));
+    }
+
+    @Test
+    public void emptyStringTest() {
+        assertEquals("", MakeSnake.makeSnake(""));
+    }
+}
